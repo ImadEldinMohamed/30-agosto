1534cf63c2338d3dac369ceb5b90bc1248c14908
ImadEldinMohamed <126964061+ImadEldinMohamed@users.noreply.github.com>
2023-08-30 16:56:56 +0200

    Aggiungere i file di progetto.

---
+++ 30 agosto/Contocorrente.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30_agosto
+{
+    internal class Contocorrente
+    {
+
+        public string Nome{ get; set; }
+
+        public string Cognome { get; set; }
+
+        public int Saldo { get; set; }
+
+        public bool Contoopen=false;
+
+
+
+        public Contocorrente() { }
+        public Contocorrente(string nome,string cognome ) {
+             this.Nome=nome;
+                this.Cognome = cognome;
+                
+                
+                
+                }
+
+        public void Aperturaconto(double totale)
+        {
+            Saldo = 0;
+            if (totale >= 1000)
+            {
+                Contoopen = true;
+                versamento(totale);
+            }
+            else Console.WriteLine("Versa di piu");
+
+        }
+
+        public double versamento(double totale)
+        {
+            return Saldo = (int)(Saldo + totale);
+        }
+
+
+        public double prelievo(double totale)
+        {
+            return Saldo = (int)(Saldo - totale);
+        }
+
+        public string Stato()
+        {
+            return $"conto di {Nome} {Cognome} e hai {Saldo}";
+
+    }
+}
+}
+
---
+++ 30 agosto/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30_agosto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Contocorrente contocorrente = new Contocorrente();
+            contocorrente.Nome = "pino";
+            contocorrente.Cognome = "gino";
+            contocorrente.Saldo = 500;
+            Console.WriteLine(contocorrente.Saldo);
+          
+
+            Contocorrente contocorrente1 = new Contocorrente();
+            Console.WriteLine("nome intestatario");
+            string nome=Console.ReadLine();
+            Console.WriteLine("cognome intestatario");
+            string cognome = Console.ReadLine();
+            Console.WriteLine("conto");
+          
+            contocorrente1.Nome = "pippo";
+            contocorrente1.Cognome = "baudo";
+            contocorrente1.Aperturaconto(5000);
+            contocorrente1.prelievo(522);
+            contocorrente1.versamento(9555);
+            Console.WriteLine(contocorrente1.Stato());
+
+            
+
+
+
+            //esercizio 2 array
+
+
+
+            string n;
+            string[] array = new string[8];
+            for(int i = 0; i < array.Length; i++)
+            {
+
+                Console.WriteLine("nome");
+                n = Console.ReadLine();
+                array[i] = n;
+            }
+          
+        }
+    }
+}
